b17074d838657430c46651e146b8c6c0647053fb
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-09-03 12:28:50 +0530

    Added Views and Public Dir

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
+// Setting Views to PUG
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
+
+// Setting Public Dir
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
-	res.send("Hello Express");
+	res.render("home");
 });
 
 app.listen(3000, () => console.log("Listening on PORT : 3000"));
